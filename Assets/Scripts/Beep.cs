38d38fdbb31f3cfa3943d4470951ea52c6e971f7
Yuri Cruz <yuriaicruz@gmail.com>
2018-10-01 23:30:22 -0300

    simple beeb and metronome

---
+++ Assets/Scripts/Beep.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class Beep : MonoBehaviour
+    {
+        private AudioSource _audioSource;
+
+        public int position = 0;
+        public int samplerate = 44100;
+        public float frequency = 440;
+
+        private void Start()
+        {
+            _audioSource = GetComponent<AudioSource>();
+            _audioSource.clip = AudioClip.Create("", 1000, 1, samplerate, false, OnAudioRead, OnAudioSetPosition);
+
+            Metronome.Beat += DoBeep;
+        }
+
+        private void OnAudioSetPosition(int i)
+        {
+            position = i;
+        }
+
+        private void OnAudioRead(float[] data)
+        {
+            int count = 0;
+            while (count < data.Length)
+            {
+                data[count] = Mathf.Sin(2 * Mathf.PI * frequency * position / samplerate);
+                position++;
+                count++;
+            }
+        }
+
+        private void DoBeep(int i)
+        {
+                _audioSource.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/Metronome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Metronome : MonoBehaviour
+    {
+        public int Tempo = 4;
+        public int Bpm = 60;
+
+        public static event Action<int> Beat;
+
+        private void Start()
+        {
+            StartCoroutine(Counter());
+        }
+
+        IEnumerator Counter()
+        {
+            var i = 0;
+            while (true)
+            {
+                yield return new WaitForSeconds(60 / (float) Bpm);
+                if (Beat != null) Beat(i);
+                i = (i + 1) % Tempo;
+            }
+        }
+    }
+}
