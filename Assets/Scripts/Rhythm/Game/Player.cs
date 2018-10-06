b2248cfbabb0fe105556cd0eae4e82f66360a14f
Yuri Cruz <yuriaicruz@gmail.com>
2018-10-06 02:10:34 -0300

    grid tests

---
+++ Assets/Scripts/Rhythm/Game/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace.Game
+{
+    public class Player : MonoBehaviour
+    {
+        private void Awake()
+        {
+            
+        }
+
+        private void Start()
+        {
+            Metronome.Beat += Beat;
+        }
+
+        private void Update()
+        {
+            GetInput();
+        }
+
+        private void GetInput()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                DoAction();
+            }
+        }
+
+        private void Beat(int index)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void DoAction()
+        {
+            
+        }
+    }
+}
